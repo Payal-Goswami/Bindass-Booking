d6d77ad55cb041214a4b363cd69a5d85def8aa9b
Payal Goswami <payalgoswami9711@gmail.com>
2026-02-03 20:30:53 +0530

    deleted files

--- frontend/src/services/api.js
+++
@@ -1,7 +0,0 @@
-const API_BASE = 'http://localhost:8080';
-
-export async function fetchResources() {
-  const res = await fetch(`${API_BASE}/resources`);
-  if (!res.ok) throw new Error('Failed to fetch resources:(');
-  return res.json();
-}
