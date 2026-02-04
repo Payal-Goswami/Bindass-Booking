e22b302bce9ce48fa209b9b3bb47dbc52bd78f10
Payal Goswami <payalgoswami9711@gmail.com>
2026-02-04 13:41:42 +0530

    modified cancelBooking.service.js

--- backend/src/modules/cancelBooking/cancelBooking.service.js
+++ backend/src/modules/cancelBooking/cancelBooking.service.js
@@ -1,9 +1,13 @@
 import { supabase } from '../../config/supabase.js';
 export async function cancelBooking({ bookingId, userId }) {
+  console.log("bookingId : ", bookingId);
   const { error } = await supabase.rpc('cancel_booking', {
     p_booking_id: bookingId,
     p_user_id: userId
   });
 
-  if (error) throw error;
+  if (error) {
+    console.log("ERROR in service.js : ", error);
+    throw error;
+  }
 }
