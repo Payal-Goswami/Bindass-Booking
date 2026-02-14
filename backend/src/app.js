47ec18352d5fae587a12f9001b964fc1419ab13e
Payal Goswami <payalgoswami9711@gmail.com>
2026-02-14 09:12:54 +0530

    clean

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,12 +7,6 @@ import userBookingRoutes from './modules/userBooking/userBooking.routes.js';
 import addResourceRoutes from './modules/addResource/addResource.routes.js';
 import { errorHandler } from './middlewares/error.js';
 const app = express();
-
-// app.use(cors({
-//   origin: 'http://localhost:5173'
-// }));
-// app.use(cors());
-
 app.use(cors({
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
@@ -22,7 +16,6 @@ app.use(cors({
 app.use(express.json());
 
 app.use('/userBookings', userBookingRoutes);
-// app.use('/availability', availabilityRoutes);
 app.use('/resources', resourceRoutes);
 app.use('/bookings', bookingRoutes);
 app.use('/cancel', cancelRoutes);
--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,8 +1,3 @@
-// export function authenticate(req, res, next) {
-//   req.user = { id: '4f036a94-fdf3-4bd6-953c-59ab6394d215', role: 'ADMIN' };
-//   next();
-// }
-
 import { supabase } from '../config/supabase.js';
 export const authenticate = async (req, res, next) => {
   try {
