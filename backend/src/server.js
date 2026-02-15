535202605e9b0d97ca7b053603fc31d470662638
Payal Goswami <payalgoswami9711@gmail.com>
2026-02-15 22:03:30 +0530

    update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import app from './app.js';
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
