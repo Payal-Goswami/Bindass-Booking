30d4fe3519337a00d8b17116806a82f2dd7dccc1
Payal Goswami <payalgoswami9711@gmail.com>
2026-02-15 21:47:13 +0530

    update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-
-import Home from './pages/Home';
-import Availability from './pages/Availability';
-import MyBookings from './pages/MyBookings';
-import AddResource from './pages/AddResource';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Availability from "./pages/Availability";
+import MyBookings from "./pages/MyBookings";
+import AddResource from "./pages/AddResource";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 export default function App() {
   return (
     <>
       <Navbar />
-
-      <div style={{ padding: '20px' }}>
+      <div style={{ padding: "20px" }}>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
@@ -21,6 +19,7 @@ export default function App() {
           <Route path="/resources/:resourceId" element={<Availability />} />
           <Route path="/my-bookings" element={<MyBookings />} />
           <Route path="/add-resource" element={<AddResource />} />
+          <Route path="/availability/:resourceId" element={<Availability />} />
         </Routes>
       </div>
     </>
